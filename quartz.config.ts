8ca9208f73d4c75a6a12da756d7ea14ca2129324
oy-hotwater <261808538+oy-hotwater@users.noreply.github.com>
2026-03-07 04:42:16 +0900

    fix: baseUrlの設定を自身のGitHub PagesのURLに修正

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "ja-JP",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://oy-hotwater.github.io/my-learning-notes/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
